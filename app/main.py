294a05b4e1e34906609b7a373336948ab6e85087
nemish bhuva <nemishbhuva01@gmail.com>
2025-07-10 21:08:59 +0530

    minor change

--- app/main.py
+++ app/main.py
@@ -13,8 +13,7 @@ app = FastAPI(
 )
 
 origins = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
+    "https://cluster-compress-frontend.vercel.app/"
 ]
 app.add_middleware(
     CORSMiddleware,
